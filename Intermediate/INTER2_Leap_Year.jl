5eb5807101f70ef99928379f76bedc4e3960744c
atott <40902466+atott@users.noreply.github.com>
2021-03-13 20:53:20 -0600

    Add files via upload

---
+++ Intermediate/INTER2_Leap_Year.jl
@@ -0,0 +1,20 @@
+
+planets = [["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"],
+	  [87.97, 224.70, 365.2422, 1.88, 11.86, 29.46, 84.01, 164.79, 248.59 ]]
+
+
+
+function leapYear(num)
+    y = 0
+    p = 0
+    while p < 1 
+	p = planets[2][num] % 1 * y
+        y += 1
+    end
+  println("$(planets[1][num]) Leap Year Every $(y-2) Years")
+end
+
+
+for i in 1:9
+  leapYear(i)
+end
